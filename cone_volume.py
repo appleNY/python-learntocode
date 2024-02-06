0491ce59ddb688963ab54cbd4a947688bd4f2a4e
edita <pub.jinson@gmail.com>
2024-02-06 17:41:59 +0900

    add cone volume

--- cone_volume.py
+++
@@ -1,8 +0,0 @@
-PI = 3.1415922653689793
-
-radius =  int(input())
-height = int(input())
-
-volume = (PI * (radius **2) * height) /3
-
-print(volume)
---
+++ telemaketers_3.py
@@ -0,0 +1,18 @@
+num1 = int(input())
+if num1 != 8 and num1 != 9:
+    print('answer')
+    exit()
+
+num2 = int(input())
+num3 = int(input())
+if num2 != num3:
+    print('answer')
+    exit()
+
+num4 = int(input())
+if num4 != 8 and num4 != 9:
+    print('answer')
+    exit()
+
+else:
+    print('ignore')
