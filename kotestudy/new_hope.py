6b1787209c03dc2d266e768bffbf2fc1ac3402fa
appleNY <pub.jinson@gmail.com>
2024-06-25 17:22:32 +0900

    move

---
+++ kotestudy/new_hope.py
@@ -0,0 +1,5 @@
+N = input()
+S = 'A long time ago in a galaxy ' + 'far, ' * int(N)
+T = S.strip(', ')
+star_wars = T + ' away...'
+print(star_wars)
---
+++ kotestudy/next_line.py
@@ -0,0 +1,5 @@
+Y = input()
+M = input()
+B = int(M) - int(Y)
+oldest_age = int(M) + int(B)
+print(oldest_age)
