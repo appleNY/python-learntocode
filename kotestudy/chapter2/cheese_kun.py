c548074ee53433f744fc4ec541b6dc706213bb3b
appleNY <pub.jinson@gmail.com>
2024-06-30 14:52:23 +0900

    링크 추가 / 오류 수정

--- kotestudy/chapter2/cheese_kun.py
+++ kotestudy/chapter2/cheese_kun.py
@@ -1,5 +1,11 @@
+#https://dmoj.ca/problem/dmopc16c1p0
+
 width = int(input())
+if width < 1 or width > 3:
+    exit()
 extra_cheese = int(input())
+if extra_cheese < 0 or extra_cheese > 100:
+    exit()
 
 if width == 3 and extra_cheese >= 95:
     a = 'absolutely'
