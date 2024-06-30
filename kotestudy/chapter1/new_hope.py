878aa0fd502f8dd38fc2f9b7d45c145b2cb6da22
appleNY <pub.jinson@gmail.com>
2024-06-30 13:44:29 +0900

    링크 추가 / 제목 수정

--- kotestudy/chapter1/new_hope.py
+++ kotestudy/chapter1/new_hope.py
@@ -1,5 +1,10 @@
-N = input()
-S = 'A long time ago in a galaxy ' + 'far, ' * int(N)
-T = S.strip(', ')
+#https://dmoj.ca/problem/wc15c2j1
+
+N = int(input())
+if N < 1 or N > 5:
+    exit()
+
+S = 'A long time ago in a galaxy ' + 'far, ' * N
+T = S.strip(', ') #마지막 쉼표 제거를 위한 코드
 star_wars = T + ' away...'
 print(star_wars)
