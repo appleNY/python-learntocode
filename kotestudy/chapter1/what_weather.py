1100fa8ec874d9a429c3c572953bc79e436b9a94
appleNY <pub.jinson@gmail.com>
2024-06-30 13:58:27 +0900

    링크 추가 / 주석 추가

--- kotestudy/chapter1/what_weather.py
+++ kotestudy/chapter1/what_weather.py
@@ -1,3 +1,10 @@
-c = int(input())
-f = (1.8 * c) + 32
-print(f)
+#https://dmoj.ca/problem/wc17c1j2
+
+celsius = int(input())
+if celsius < -40 or celsius > 40:
+    exit()
+
+fahrenheit = (1.8 * celsius) + 32
+#celsius = 5/9 * (fahrenheit - 32)
+
+print(fahrenheit)
