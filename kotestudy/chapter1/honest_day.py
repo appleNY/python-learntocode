870f96143d4c0b977fe23250abd8cce4e8f842da
appleNY <pub.jinson@gmail.com>
2024-06-30 14:25:10 +0900

    링크 추가 / 주석 추가

--- kotestudy/chapter1/honest_day.py
+++ kotestudy/chapter1/honest_day.py
@@ -1,6 +1,11 @@
-P = int(input())
-B = int(input())
-D = int(input())
+#https://dmoj.ca/problem/wc18c3j1
+
+P = int(input()) #litres of leftover paint
+B = int(input()) #litres of paint which artfully covered on bottlecap
+D = int(input()) #Pokédollar earned from selling badges
+
+if P < 1 or P > 100 and B < 1 or B > 100 and D < 1 or D > 100:
+    exit()
 
 earning_money = ((P // B) * D) + (P % B)
 print(earning_money)
