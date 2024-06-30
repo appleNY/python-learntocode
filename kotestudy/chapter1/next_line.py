f78dd13b38e4f661703b4577c917b9643cbd9137
appleNY <pub.jinson@gmail.com>
2024-06-30 13:52:54 +0900

    링크 추가 / 조건 추가

--- kotestudy/chapter1/next_line.py
+++ kotestudy/chapter1/next_line.py
@@ -1,5 +1,10 @@
-Y = input()
-M = input()
-B = int(M) - int(Y)
-oldest_age = int(M) + int(B)
+#https://dmoj.ca/problem/ccc13j1
+
+youngest_age = int(input())
+middle_age = int(input())
+if youngest_age < 0 or youngest_age > 50 and middle_age < youngest_age or middle_age > 50:
+    exit()
+
+age_gap = middle_age - youngest_age
+oldest_age = middle_age + age_gap
 print(oldest_age)
