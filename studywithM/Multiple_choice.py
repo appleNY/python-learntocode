3b1639ad2b505ed0cdab301b2a75609089b77419
appleNY <pub.jinson@gmail.com>
2024-07-14 15:46:31 +0900

    링크 추가 / 주석 추가

--- studywithM/Multiple_choice.py
+++ studywithM/Multiple_choice.py
@@ -1,3 +1,5 @@
+#https://dmoj.ca/problem/ccc11s2
+
 student_num = int(input())
 if student_num < 0 or student_num > 10000:
     exit()
@@ -5,17 +7,17 @@ if student_num < 0 or student_num > 10000:
 correct_answer = 0
 for i in range(2):
     for j in range(student_num):
+        student1 = input()
+        student2 = input()
+        student3 = input()
         if i == 0:      #student answer
             if j == 0:
-                student1 = input()
                 if len(student1) != 1 or "ABCDE".find(student1) == -1:
                     exit()
             elif j == 1:
-                student2 = input()
                 if len(student2) != 1 or "ABCDE".find(student2) == -1:
                     exit()
             elif j == 2:
-                student3 = input()
                 if len(student3) != 1 or "ABCDE".find(student3) == -1:
                     exit()
         else:           #correct answer
