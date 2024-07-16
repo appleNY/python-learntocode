e006ac368fdbaa098e847602389f04969363d892
appleNY <pub.jinson@gmail.com>
2024-07-16 22:24:42 +0900

    링크 추가 / 주석 추가

--- kotestudy/chapter4/ameri_canadian.py
+++ kotestudy/chapter4/ameri_canadian.py
@@ -9,25 +9,19 @@
 # N lines of words
 # quit!
 
-canadian_words = []
+american_words = []
 
 while True:
     american_words = input()
     if american_words == "quit!":
         exit()
-    canadian_words.append(american_words)
-    # len(word) < 64
+   # len(words) < 64
 
-    index = 0
-    #american_vowel = "a, e, i,o, u, y"
-    while index < len(canadian_words):
-        american_words = canadian_words[index]
-        if len(american_words) >= 4 and american_words[-2:] == "or" and american_words[-3] != "a, e, i, o, u, y":
-            canadian_words[index] = american_words[:-2] + "our"
-        index += 1
-
-# output:
-# replaced words
-
-    for american_words in canadian_words:
+    if len(american_words) >= 4 and american_words[-3:-2] != "a, e, i, o, u, y" and american_words[-2:] == "or":
+        canadian_words = american_words[:-2] + "our"
+        # output:
+        # replaced words
         print(canadian_words)
+    else:
+        print(american_words)
+
