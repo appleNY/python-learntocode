01e58ce8ad0843dc093b11357c4b4598958385ac
appleNY <pub.jinson@gmail.com>
2024-07-16 15:48:38 +0900

    링크 추가 / 주석 추가

--- kotestudy/chapter4/epidemiology_disease.py
+++ kotestudy/chapter4/epidemiology_disease.py
@@ -1,24 +1,29 @@
-#https://dmoj.ca/problem/ccc20j2
+# https://dmoj.ca/problem/ccc20j2
 
-#input:
-#total amount of people
-#number of people who infected on day 1
-#number of people infected the next day(day 2)
+# input:
+# total amount of people, P
+# number of people who infected on day0, N
+# infected growth rate, R
 
-P = total_amount_people = int(input())  #P
-N = infected_people_day0 = int(input())  #N
-R = infected_people_next_day = int(input())  #R
+P = total_amount_people = int(input())
+N = infected_people_day0 = int(input())
+R = infected_growth_rate = int(input())
 
+# P <= 10 ** 7
+# N <= P
+# R <= 10
+
+total_infected = N  # current_infected
 days = 0
 
-while P < N + (R * days):
+# output:
+# number of the first day on which the total number of infected people is greater than P
+
+while total_infected <= P:
     days = days + 1
-    R = R * days
-    if
+    total_infected = total_infected + (N * (R ** days))
+# repeat until the total number of infected people is equal to or less than P
+# R times the number of infections per day
+# total infected people is N + N(current infected people) * (growth rate * growth rate * growth rate...)
 
-
-
-
-
-
-#output: days until the entire population is infected
+print(days)
